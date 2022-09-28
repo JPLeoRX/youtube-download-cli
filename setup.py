59a1787d628456660191dd472c540fa9a1c7d21c
jpleorx <leo.ertuna@gmail.com>
2022-09-28 21:16:16 +0300

    Setup

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+
+with open("README.md", "r") as readme_file:
+    readme_text = readme_file.read()
+
+
+setup_args = dict(
+    entry_points = {
+        'console_scripts': ['youtube-download-cli=youtube_download_cli.command_line:main'],
+    },
+    name='youtube-download-cli',
+    version='0.0.0.1',
+    description="",
+    keywords=[],
+    long_description=readme_text,
+    long_description_content_type="text/markdown",
+    license='Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International Public License',
+    packages=find_packages(),
+    author="Leo Ertuna",
+    author_email="leo.ertuna@gmail.com",
+    url="https://github.com/jpleorx/youtube-download-cli",
+    download_url='https://pypi.org/project/youtube-download-cli/'
+)
+
+
+install_requires = [
+    'argparse',
+    'colorama',
+    'pytube'
+]
+
+
+if __name__ == '__main__':
+    setup(**setup_args, install_requires=install_requires)
